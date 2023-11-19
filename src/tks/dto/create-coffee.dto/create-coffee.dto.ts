32bff33c27c23656a6a94037711f5032a75b34a2
TK <937109861@qq.com>
2023-11-19 11:21:51 +0800

    new-est

--- src/tks/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/tks/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,6 +1,7 @@
-import {IsString} from 'class-validator';
+import {IsNumber, IsString} from 'class-validator';
 
 export class CreateCoffeeDto {
+    @IsNumber()
     readonly id:number;
 
     @IsString()
