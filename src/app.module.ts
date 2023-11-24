c3f51a5c0221dff9baafd31bfe8edf1ba76ef341
TK <937109861@qq.com>
2023-11-24 17:25:21 +0800

    g

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { TksController } from './tks/tks.controller';
 import { TksService } from './tks/tks.service';
 import { TksModule } from './tks/tks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TksModule],
+  imports: [TksModule,TypeOrmModule.forRoot(
+    {
+      type:'postgres',
+      host:'localhost',
+      port:5432,
+      username:'postgres',
+      password:'123123',
+      database:'postgres',
+      autoLoadEntities:true,
+      synchronize:true,
+    }
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
